b9bd0d25ae8ba8ced52e95211a7c2d16a943812e
Gunaseelan123 <gunaseelanramakrishnan123@gmail.com>
2025-04-17 13:36:33 +0530

    first commit

---
+++ src/components/Footer.jsx
@@ -0,0 +1,30 @@
+import { FaGithub, FaLinkedin, FaInstagram } from "react-icons/fa";
+
+const Footer = () => {
+  return (
+    <footer className="bg-[#00ADB5] text-white py-6">
+      <div className="max-w-[90%] md:max-w-[75%] mx-auto text-center space-y-2">
+        <p className="text-sm md:text-base font-medium">
+          © {new Date().getFullYear()} Gunaseelan. All rights reserved.
+        </p>
+        <div className="flex justify-center gap-4 mt-2">
+        <a
+            href="https://github.com/Gunaseelan123"
+            className="hover:text-[#101010]"
+          >
+            <FaGithub size={29} />
+          </a>
+          <a
+            href="https://www.linkedin.com/in/gunaseelan-r-05a0b0253/"
+            className="hover:text-[#0d0d0d]"
+          >
+            <FaLinkedin size={29} />
+          </a>
+
+        </div>
+      </div>
+    </footer>
+  );
+};
+
+export default Footer;
